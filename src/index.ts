ebc6f42c3668ced39b0be26733f9c80ac2b3ba70
WallopChuchart <38816833+Mxsterloot@users.noreply.github.com>
2025-01-19 22:39:11 +0700

    Refactor project structure: rename app, add dependencies, and integrate middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { Elysia } from "elysia";
-
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { cors } from "@elysiajs/cors";
+import { routes } from "./routes";
+const app = new Elysia().use(swagger()).use(cors()).use(routes).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+export type App = typeof app;
---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import { Elysia, t } from "elysia";
+
+export const routes = new Elysia().get("/api/users", () => {
+  return [
+    {
+      name: "John Doe",
+      email: "johndoe@example.com",
+      age: 25,
+    },
+    {
+      name: "Erik Doe",
+      email: "jane@example.com",
+      age: 22,
+    },
+  ];
+});
